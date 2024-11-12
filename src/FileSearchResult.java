c11152d9f9b149cdbcc1d60c3a33455b83a9601a
tiagomonteiroo <167870432+tiagomonteiroo@users.noreply.github.com>
2024-11-12 19:48:23 +0000

    Pesquisa Ficheiros(filesearch)

--- src/FileSearchResult.java
+++ src/FileSearchResult.java
@@ -1,7 +1,19 @@
 public class FileSearchResult {
-    private WordSearchMessage wordSearchMessage;
+    private WordSearchMessage palavraChave;
+    //private String hash;
     private long tamanhoFicheiro;
     private String nomeFicheiro;
     private String addrNo;
     private int portaNo;
+
+    public FileSearchResult(WordSearchMessage palavraChave, long tamanhoFicheiro, String nomeFicheiro, String addrNo, int portaNo){
+        this.palavraChave=palavraChave;
+        this.tamanhoFicheiro=tamanhoFicheiro;
+        //this.hash = hash;
+        this.nomeFicheiro=nomeFicheiro;
+        this.addrNo=addrNo;
+        this.portaNo=portaNo;
+    }
+
+
 }
