371e0d682c477a1cb520556d6e39c04a94992f99
jxbarbosax <jxbarbosax@gmail.com>
2024-11-13 15:18:23 +0000

    hellu

--- src/FileSearchResult.java
+++ src/FileSearchResult.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class FileSearchResult {
     private WordSearchMessage palavraChave;
     //private String hash;
@@ -6,6 +8,8 @@ public class FileSearchResult {
     private String addrNo;
     private int portaNo;
 
+    private JList<String> lista;
+
     public FileSearchResult(WordSearchMessage palavraChave, long tamanhoFicheiro, String nomeFicheiro, String addrNo, int portaNo){
         this.palavraChave=palavraChave;
         this.tamanhoFicheiro=tamanhoFicheiro;
@@ -13,5 +17,16 @@ public class FileSearchResult {
         this.nomeFicheiro=nomeFicheiro;
         this.addrNo=addrNo;
         this.portaNo=portaNo;
+    }
+
+    public JList<String> ficheirosComPalavraChave(String palavraChave) {
+        DefaultListModel<String> resultado = new DefaultListModel<>();
+        for (int i = 0; i < lista.getModel().getSize(); i++) {
+            String ficheiro = lista.getModel().getElementAt(i);
+            if (ficheiro.contains(palavraChave)) {
+                resultado.addElement(ficheiro);
+            }
+        }
+        return new JList<>(resultado);
     }
 }
--- src/WordSearchMessage.java
+++ src/WordSearchMessage.java
@@ -1,5 +1,8 @@
+import javax.swing.*;
+
 public class WordSearchMessage {
     private String palavraChave;
+    private JList<String> lista;
 
     public WordSearchMessage(String palavraChave){
         this.palavraChave = palavraChave;
